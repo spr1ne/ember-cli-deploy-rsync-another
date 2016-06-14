fea30cecebf218eec01e06fb5ec12117ca2b61ef
RD <dampilon.robert@gmail.com>
2016-06-14 13:14:55 +0600

    Default configuration

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-/* jshint node: true */
-'use strict';
+var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
-  name: 'ember-cli-deploy-ssh'
+  name: 'ember-cli-deploy-ssh',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        filePattern: '**/*.{js,css,png}' // default filePattern if it isn't defined in config/dpeloy.js
+      },
+
+      requiredConfig: ['username', 'host', 'remoteDir', 'privateKeyFile'],
+
+      didBuild: function(context) {
+        //do something amazing here once the project has been built
+      },
+
+      upload: function(context) {
+        this.log('Uploading assets');
+        //do something here to actually deploy your app somewhere
+      },
+
+      didDeploy: function(context) {
+        //do something here like notify your team on slack
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
